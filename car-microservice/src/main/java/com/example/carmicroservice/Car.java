d9112eb3df1589ef6ed6d21313435d46ea86abd5
Kacper Zielinski <kacper.zielinski@mailfence.com>
2021-01-18 17:44:04 +0100

    Added Lombok for project

--- car-microservice/src/main/java/com/example/carmicroservice/Car.java
+++ car-microservice/src/main/java/com/example/carmicroservice/Car.java
@@ -1,13 +1,12 @@
 package com.example.carmicroservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Car {
     private String company;
     private String model;
     private String engine;
-
-    public Car(String company, String model, String engine) {
-        this.company = company;
-        this.model = model;
-        this.engine = engine;
-    }
 }
