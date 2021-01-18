ec3862e2dbfadb11e5870af259fdab4e38febb6b
Kacper Zielinski <kacper.zielinski@mailfence.com>
2021-01-18 18:18:42 +0100

    Added Spring Cloud Config Server

---
+++ spring-cloud-server-config/src/test/java/com/example/springcloudserverconfig/SpringCloudServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudserverconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudServerConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
